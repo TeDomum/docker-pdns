cc401f4ee35f0a67b10c0eb09ec19778b1af6b1a
Pierre Jaury <pierre@jaury.eu>
2015-07-28 14:13:26 +0200

    Handle dynamic pdns_server arguments

--- start.sh
+++ start.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
+SERVER_ARGS=`env|grep '^DNS_'|cut -c 5-|sed 's,_,-,g;s,^,--,'|tr "\n[:upper:]" " [:lower:]"`
+
 pdns_server \
 --daemon=no \
 --master=yes \
 --slave=yes \
 --webserver=yes \
 --disable-axfr=no \
---allow-axfr-ips=${PDNS_AXFR} \
---gmysql-host=db \
---gmysql-dbname=${PDNS_MYSQL_DB} \
---gmysql-user=${PDNS_MYSQL_USER} \
---gmysql-password=${PDNS_MYSQL_PASSWORD}
+${SERVER_ARGS}
